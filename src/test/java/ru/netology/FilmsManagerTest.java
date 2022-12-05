fa8737a0d0279420fee25252270e71827c747bb0
Nataliia Ilnytska <lelya_antonenko@bk.ru>
2022-12-05 22:09:07 +0300

    add tests

--- src/test/java/ru/netology/FilmsManagerTest.java
+++ src/test/java/ru/netology/FilmsManagerTest.java
@@ -22,7 +22,7 @@ public class FilmsManagerTest {
     }
 
     @Test
-    public void shouldFindLast() {
+    public void shouldFindLastFewerThanLimit() {
         FilmsManager manager = new FilmsManager();
 
         manager.addFilm("Бладшот 1");
@@ -34,6 +34,50 @@ public class FilmsManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldFindLastEqualToLimit() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Бладшот 1");
+        manager.addFilm("Бладшот 2");
+        manager.addFilm("Бладшот 3");
+        manager.addFilm("Матрица 1");
+        manager.addFilm("Матрица 2");
+        manager.addFilm("Матрица 3");
+        manager.addFilm("Матрица 4");
+        manager.addFilm("Миссия невыполнима");
+        manager.addFilm("Титаник");
+        manager.addFilm("Храброе сердце");
+
+        String[] expected = {"Храброе сердце", "Титаник", "Миссия невыполнима", "Матрица 4", "Матрица 3", "Матрица 2", "Матрица 1", "Бладшот 3", "Бладшот 2", "Бладшот 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastExceedsLimit() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("Бладшот 1");
+        manager.addFilm("Бладшот 2");
+        manager.addFilm("Бладшот 3");
+        manager.addFilm("Матрица 1");
+        manager.addFilm("Матрица 2");
+        manager.addFilm("Матрица 3");
+        manager.addFilm("Матрица 4");
+        manager.addFilm("Миссия невыполнима");
+        manager.addFilm("Титаник");
+        manager.addFilm("Храброе сердце");
+        manager.addFilm("Криминальное чтиво");
+
+        String[] expected = {"Криминальное чтиво", "Храброе сердце", "Титаник", "Миссия невыполнима", "Матрица 4", "Матрица 3", "Матрица 2", "Матрица 1", "Бладшот 3", "Бладшот 2"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 
 }
     
